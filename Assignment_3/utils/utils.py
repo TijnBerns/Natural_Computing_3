fcef86201f87f6ca51057de78c5cad2e263c6ee1
TijnBerns <tijn_berns@hotmail.com>
2022-03-18 09:57:41 +0100

    Added utility method for calculating average movement of cells

---
+++ Assignment_3/utils/utils.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np
+import os
+import math
+from pathlib import Path
+
+
+def getAverageMovement(file: str):
+    df = pd.read_csv(file, sep='\t')
+    average_movements = []
+
+    for id in pd.unique(df["id"]):
+        centroids = df[df["id"] == id][['x','y']].to_numpy(np.float32)
+        movements = np.array([np.linalg.norm(centroids[i] - centroids[i+1]) for i in range(len(centroids) - 1)])
+        average_movement = np.average(movements)
+        
+        # Check for nan value which may result in case of collapsed cells
+        if not math.isnan(average_movement):
+            average_movements.append(np.average(movements))
+            
+    return np.average(average_movements)
+        
+                
+def main():
+    root = Path("../logs/")
+    results = [] 
+    
+    for f in  root.rglob("*.csv"):
+        results.append([f, getAverageMovement(f)])
+        
+    df = pd.DataFrame(results, columns=["File", "Avg. movement"])
+    print(df)
+
+
+if __name__ == "__main__":
+    main()
