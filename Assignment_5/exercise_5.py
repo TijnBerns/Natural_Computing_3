50995d31a8c4c69af3a4659231d357d25b17e582
TijnBerns <tijn_berns@hotmail.com>
2022-04-21 15:46:28 +0200

    added scikit-learn implementation

--- Assignment_5/exercise_5.py
+++ Assignment_5/exercise_5.py
@@ -1,10 +1,13 @@
+
+from email.mime import base
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
-
+from sklearn.ensemble import RandomForestClassifier
+from tqdm import tqdm
 
 
 def load_data(fname):
@@ -18,16 +21,16 @@ def load_data(fname):
     """
     # Read csv file
     data = pd.read_csv(fname,  header=None).sample(frac=1, random_state=0)
-    
+
     # Transform labels into integers
     enc = LabelEncoder()
     enc.fit(data[1])
     data[1] = enc.transform(data[1])
-    
+
     # Split into train and test set and return
     data = np.array(data)
     train, test = np.split(data, [int(0.8*len(data))])
-    return (train[:, 2:].astype('float32'), train[:, 1].astype('int'), 
+    return (train[:, 2:].astype('float32'), train[:, 1].astype('int'),
             test[:, 2:].astype('float32'), test[:, 1].astype('int'))
 
 
@@ -53,13 +56,13 @@ def evaluate_pred(y_true, y_pred):
 
 
 def ensamble(X_train, y_train, X_test, n_trees, max_depth, max_leaf_nodes):
-    """Computes the prediction of an ensamble of decision trees 
+    """Computes the prediction of an ensamble of decision trees
 
     Args:
         X_train: Training dataset
         y_train: Labels corresponding to the training dataset
         X_test: Test dataset
-        n_trees: Number of trees in ensamble 
+        n_trees: Number of trees in ensamble
         max_depth: Max depth of a single tree
         max_leaf_nodes: Maximum number of leafs of a single tree
 
@@ -71,18 +74,23 @@ def ensamble(X_train, y_train, X_test, n_trees, max_depth, max_leaf_nodes):
     y_train_split = np.array_split(y_train, n_trees)
 
     for (X, y) in zip(X_train_split, y_train_split):
-        clf = DecisionTreeClassifier(max_depth=max_depth, max_leaf_nodes=max_leaf_nodes)
+        clf = DecisionTreeClassifier(
+            max_depth=max_depth, max_leaf_nodes=max_leaf_nodes)
         clf.fit(X, y)
         y_pred += clf.predict(X_test)
     return np.round(y_pred / n_trees)
 
 
-def ex5(X_train, y_train, X_test, y_test, max_depth, max_leaf_nodes):
-    res = []
-    for n in range(1, 300):
-        y_pred = ensamble(X_train, y_train, X_test, n, max_depth, max_leaf_nodes)
-        res.append(evaluate_pred(y_test, y_pred)['accuracy'])
-        
+def ex5_01(max_depth, max_leaf_nodes, restarts=40, max_trees=100):
+    res = np.zeros((restarts, max_trees-1))
+    for m in tqdm(range(restarts)):
+        X_train, y_train, X_test, y_test = load_data(fname)
+        for n in range(1, max_trees):
+            y_pred = ensamble(X_train, y_train, X_test, n,
+                              max_depth, max_leaf_nodes)
+            res[m][n-1] = evaluate_pred(y_test, y_pred)['accuracy']
+    res = np.mean(res, axis=0)
+
     plt.figure()
     plt.title(f"Max depth {max_depth}, Max leaf nodes {max_leaf_nodes}")
     plt.plot(res)
@@ -91,22 +99,36 @@ def ex5(X_train, y_train, X_test, y_test, max_depth, max_leaf_nodes):
     plt.show()
 
 
+def ex5_02(X_train, y_train, X_test, y_test, m_depth):
+    res = []
+    for n in tqdm(range(1, 200)):
+        clf = RandomForestClassifier(n_estimators=n, max_depth=m_depth)
+        clf.fit(X_train, y_train)
+        y_pred = clf.predict(X_test)
+        res.append(evaluate_pred(y_test, y_pred)['accuracy'])
+
+    # Print results
+    res = np.array(res)
+    baseline = res[0]
+    best = np.max(res)
+    best_ntrees = res.argmax()
+    print(f"max_depth: {m_depth}")
+    print(f"Baseline accuracy: {baseline}")
+    print(f"Best ensamble accuracy: {best} ({best_ntrees} trees)")
+
+    # Plot results
+    plt.figure()
+    plt.title(f"Max depth {m_depth}")
+    plt.plot(res)
+    plt.xlabel("Number of trees in ensamble")
+    plt.ylabel("Accuracy")
+    plt.show()
+
+    return baseline, best, best_ntrees
+
+
 if __name__ == "__main__":
     np.random.seed(2022)
     fname = "data/wdbc.csv"
     X_train, y_train, X_test, y_test = load_data(fname)
-    depth = 1
-    leafs = 2
-
-    # Baseline
-    y_pred = ensamble(X_train, y_train, X_test, 1, depth, leafs)
-    accuracy = evaluate_pred(y_test, y_pred)['accuracy']
-    print(f"Accuracy single tree:\t {accuracy}")
-
-    # Ensamble 
-    y_pred = ensamble(X_train, y_train, X_test, 10, depth, leafs)
-    accuracy = evaluate_pred(y_test, y_pred)['accuracy']
-    print(f"Accuracy ensamble:\t {accuracy}")
-    
-    # Create different plots...
-    ex5(X_train, y_train, X_test, y_test, depth, leafs)
+    ex5_02(X_train, y_train, X_test, y_test, 100)
